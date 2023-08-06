067e3345b07043ccbfdee74bd7c4eedb4168e903
Mirlanov <mirlanovdamir@gmail.com>
2023-08-06 19:22:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw_3_8/Characters.kt
@@ -0,0 +1,8 @@
+package com.example.hw_3_8
+
+
+data class Characters(
+    val name: String,
+    val state: String,
+    val image: String
+) : java.io.Serializable
---
+++ app/src/main/java/com/example/hw_3_8/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.example.hw_3_8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.hw_3_8.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+    private lateinit var navArgs: SecondFragmentArgs
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            navArgs = SecondFragmentArgs.fromBundle(it)
+        }
+        binding.tvName.text = navArgs.harry.name
+        binding.tvState.text = navArgs.harry.state
+        Glide.with(binding.imgPhotoCharacter).load(navArgs.harry.image).into(binding.imgPhotoCharacter)
+    }
+}
